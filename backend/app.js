f32150aabf11c45f8d12e30fc8e90b288a7f50c5
NotHuzan <mhuzansaleem@gmail.com>
2024-07-11 02:29:52 +0500

    update cors

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const userRouter = require("./routes/userRoutes.js");
 const carRouter = require("./routes/carRoutes.js");
 require("dotenv").config();
 
-const PORT=process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.DB_STRING)
@@ -25,7 +25,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // app.use(cors());
 app.use("/uploads", express.static("uploads"));
-app.use(cors({ credentials: true, origin: process.env.CORS_ORIGIN || "http://localhost:3000" }));
+app.use(
+  cors({
+    origin: "*", // Allow all origins
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 //routers
