26ed4a68f257cdfb485a6447b54fa0f6f6111847
NotHuzan <mhuzansaleem@gmail.com>
2024-07-11 02:17:41 +0500

    update cors policy

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // app.use(cors());
 app.use("/uploads", express.static("uploads"));
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: process.env.CORS_ORIGIN || "http://localhost:3000" }));
 app.use(cookieParser());
 
 //routers
